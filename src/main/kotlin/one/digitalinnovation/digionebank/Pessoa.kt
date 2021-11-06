d37ddf1978e07321544e548df515a9a4f2fe0c6b
Lucas Gonçalves <front.end.lb@gmail.com>
2021-11-06 00:19:40 -0300

    modificador de acesso de cpf set alterado pra private

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,6 +3,11 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Lucas"
     var cpf: String = "123.456.789.10"
+        private set
+
+//    inner class Endereco {
+//        var rua: String = "Rua da Paz"
+//    }
 }
 
 fun main() {
@@ -10,4 +15,6 @@ fun main() {
 
     println(pessoa.nome)
     println(pessoa.cpf)
+
+//    println(pessoa.Endereco().rua)
 }
